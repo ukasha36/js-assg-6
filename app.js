d8afb6fdeb089df741d45cc091ed1211f0272d66
ukasha36 <ukashashabbir36@gmail.com>
2023-10-13 22:25:17 +0500

    Files added

---
+++ app.js
@@ -0,0 +1,76 @@
+// Q -- 1
+
+// function dateone( ) {
+//     const todaydate = new Date() ;
+//     console.log(todaydate);
+// }
+// dateone();
+
+// Q -- 2
+
+// function greet(){
+//     const first = prompt("Enter your first name");
+//     const last = prompt("Enter your last name");
+//     const hello = "Hello "
+//     console.log(hello + first + last);
+// }
+
+// greet();
+
+// Q -- 3
+
+// function sum(){
+//     const first1 = prompt("Enter your  num 1");
+//     const first2= +first1 ; 
+//     const last1 = prompt("Enter your num 2 ");
+//     const last2= +last1 ; 
+    
+  
+//     return first2 + last2
+
+// }
+
+//  console.log("the sum are " + sum())
+
+// Q -- 4
+
+// function calculator(num1 , num2 , operator ){
+//     if(operator=="+"){
+//         return num1 + num2 ; 
+//     }
+//     else if(operator=="-"){
+//         return num1 - num2 ; 
+//     }
+//     else if(operator=="*"){
+//         return num1 * num2 ; 
+//     }
+//     if(operator=="/"){
+//         return num1 / num2 ; 
+//     }
+// }
+//  const result = calculator(1 , 2,"+")
+//  console.log(result);
+
+// Q -- 5
+
+// function square(num ){
+   
+//     return num * num ;  
+// }
+// const result = square(10)
+// console.log(result);
+
+// Q -- 6
+
+// function count(){
+//     const start= prompt("Enter start number "); 
+//     const start2 = +start;
+//     const end = prompt("Enter end  number "); 
+//     const end2 = +end; 
+//     for ( i=start2 ; i =end2 ; i++ ) {
+//      console.log(i) ; 
+//     }
+//     return i ;
+// }
+// const result= count();
+// console.log(result);
